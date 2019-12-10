f1c51530d5f3debd4a8640723c4fe8e19c579769
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-10 14:24:30 +0100

    simplify

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,14 +4,12 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count => count + 1);
   const decrement = () => setCount(count => count - 1);
-  const reset = () => setCount(0);
 
   return (
     <div className='component'>
       The count is {count}
       <button onClick={increment}>inc</button>
       <button onClick={decrement}>dec</button>
-      <button onClick={reset}>reset</button>
     </div>
   );
 }
