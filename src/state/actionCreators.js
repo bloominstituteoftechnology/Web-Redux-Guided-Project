46f183b7daf34db03174d832ffda4b26ee3840ca
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-11 12:14:51 +0100

    wip

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -1,5 +1,16 @@
 import * as types from './actionTypes';
 // STEP 7 OF THE GENERAL STEPS
+
+// React components shouldn't worry about
+// things like actions or action types!
+// So we create these helpers
+// for the components to consume.
+
+// The arguments will be supplied
+// by the React component
+
+// The Counter component will
+// be interested in this function!
 export function increment() {
   return {
     type: types.INCREMENT,
--- src/state/reducers.js
+++ src/state/reducers.js
@@ -3,7 +3,7 @@ import * as types from './actionTypes';
 // STEP 3 OF THE GENERAL STEPS
 
 // WE HAVE SETTLED FOR 3 SLICES OF STATE
-// THIS MEANS 3 REDUCERS
+// THIS MEANS 3 REDUCERS!
 
 // REDUCER 1 - count
 const initialCount = 0;
