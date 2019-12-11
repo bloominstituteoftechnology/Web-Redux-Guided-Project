01876b555d75eb1461683b68eb195a66c2cdc8b8
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-11 11:10:21 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers } from 'redux';
+import { countReducer, todosReducer, formReducer } from './state/reducers';
 import Todos from './components/Todos';
 import Counter from './components/Counter';
 import Form from './components/Form';
-
 import './App.less';
 
-// const state = {
-//   count: 2,
-//   countSquared: 4, // NOOO
-//   constants: [],   // NOOO
-//   form: {
-//     fname: "",
-//     lname: "",
-//   },
-//   todos: [],
-// }
+const monsterReducer = combineReducers({
+  // what keys? One per slice, choosing pretty names
+  count: countReducer,
+  form: formReducer,
+  todos: todosReducer,
+});
 
-// HOW MANY SLICES ARE THERE HERE? 3
 
 ReactDOM.render(<>
   <Counter />
