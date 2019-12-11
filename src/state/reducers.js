ce26a9f507887132b351921760bdc5fca9ba8830
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-11 10:39:53 +0100

    wip

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -2,11 +2,33 @@
 // WE HAVE SETTLED FOR 3 SLICES OF STATE
 // THIS MEANS 3 REDUCERS
 
-// REDUCER 1 - COUNT
+// REDUCER 1 - count
 const initialCount = 0;
-function countReducer(count = initialCount, action) {
+export function countReducer(count = initialCount, action) {
   switch (action.type) {
     default:
       return count;
+  }
+}
+
+// REDUCER 2 - form
+const initialForm = { fname: '', lname: '' };
+export function formReducer(form = initialForm, action) {
+  switch (action.type) {
+    default:
+      return form;
+  }
+}
+
+// REDUCER 3 - todos
+const initialTodos = [
+  { id: '1', name: "sweeping", completed: false },
+  { id: '2', name: "mopping", completed: false },
+  { id: '3', name: "washing dishes", completed: false },
+];
+export function todosReducer(todos = initialTodos, action) {
+  switch (action.type) {
+    default:
+      return todos;
   }
 }
