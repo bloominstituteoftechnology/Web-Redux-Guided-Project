fb3821a0210da78c918db34ee1b7a4d01d9770ee
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-11 11:24:57 +0100

    wip

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -1,1 +1,18 @@
+import * as types from './actionTypes';
 // action creators here
+// STEP 7
+export function increment() {
+  return {
+    type: types.INCREMENT, // we need our types!!!!
+  };
+}
+
+export function decrement() {
+  return {
+    type: types.DECREMENT, // we need our types!!!!
+  };
+}
+
+// inputchange // needs args
+// mark // needs args
+// submit
