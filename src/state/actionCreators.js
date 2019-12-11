3592a5c94158273fc82b8e3aa6d9e6a4cf879164
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-11 11:42:24 +0100

    wip

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -32,3 +32,14 @@ export function markTodo(id, completed) {
     payload: { id, completed },
   };
 }
+
+// export function addTodo(text) {
+//   return {
+//     type: types.ADD_TODO,
+//     payload: {
+//       text,
+//       id: Date.now(),
+//       completed: false,
+//     },
+//   };
+// }
