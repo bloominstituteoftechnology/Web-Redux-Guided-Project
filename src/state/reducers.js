b50a8f6269b2b23a0f6e8b00392924311aff216f
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-11 10:36:54 +0100

    wip

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,1 +1,12 @@
 // reducers here
+// WE HAVE SETTLED FOR 3 SLICES OF STATE
+// THIS MEANS 3 REDUCERS
+
+// REDUCER 1 - COUNT
+const initialCount = 0;
+function countReducer(count = initialCount, action) {
+  switch (action.type) {
+    default:
+      return count;
+  }
+}
